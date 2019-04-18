3abc3f5dea66d6d040b264c3763e52ac12504986
chungheng <chungheng.yeh@gmail.com>
2019-04-17 23:59:11 -0400

    update __init__.py

--- ipyneural/__init__.py
+++ ipyneural/__init__.py
@@ -4,7 +4,7 @@
 # Copyright (c) Chung-Heng Yeh.
 # Distributed under the terms of the Modified BSD License.
 
-from .core import NeuralWidget
+from .core import NeuralWidget, MultiFigureWidget, FigureCompareWidget
 from ._version import __version__, version_info
 
 from .nbextension import _jupyter_nbextension_paths
